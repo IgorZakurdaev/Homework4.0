85bcb33173fce2fb609dcc591e0e9e0cd1edadeb
Igor Zakurdaev <zakurdaev12@mail.ru>
2024-12-28 17:07:57 +0400

    -fix after review

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -8,82 +8,98 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
-    CalendarComponent calendarComponent =new CalendarComponent();
+    CalendarComponent calendarComponent = new CalendarComponent();
+
     public RegistrationPage openPage() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        return this;
+    }
+
+    public RegistrationPage removeBanners() {
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         return this;
     }
-    private SelenideElement firstNameInput =$("#firstName"),
-    lastNameInput = $("#lastName"),
-    userEmailInput =$("#userEmail"),
-    genderWrapper =$("#genterWrapper"),
-    userNumberInput = $("#userNumber"),
-    dateOfBirthInput = $("#dateOfBirthInput"),
-    subjects =$("#subjectsInput"),
-    hobbies =$("div#hobbiesWrapper"),
-    uploadPicture=$("#uploadPicture"),
-    currentAddress=$("#currentAddress"),
-    state = $("#stateCity-wrapper"),
-    submit = $("#submit");
+
+    private SelenideElement firstNameInput = $("#firstName");
+    private SelenideElement lastNameInput = $("#lastName");
+    private SelenideElement userEmailInput = $("#userEmail");
+    private SelenideElement genderWrapper = $("#genterWrapper");
+    private SelenideElement userNumberInput = $("#userNumber");
+    private SelenideElement dateOfBirthInput = $("#dateOfBirthInput");
+    private SelenideElement subjects = $("#subjectsInput");
+    private SelenideElement hobbies = $("div#hobbiesWrapper");
+    private SelenideElement uploadPicture = $("#uploadPicture");
+    private SelenideElement currentAddress = $("#currentAddress");
+    private SelenideElement state = $("#stateCity-wrapper");
+    private SelenideElement submit = $("#submit");
 
 
-
-    public RegistrationPage setFirstName (String name){
+    public RegistrationPage setFirstName(String name) {
         firstNameInput.setValue(name);
         return this;
     }
-    public RegistrationPage setLastName (String name){
+
+    public RegistrationPage setLastName(String name) {
         lastNameInput.setValue(name);
         return this;
     }
-    public RegistrationPage setEmail (String email){
+
+    public RegistrationPage setEmail(String email) {
         userEmailInput.setValue(email);
         return this;
     }
-    public RegistrationPage gender (String value) {
+
+    public RegistrationPage gender(String value) {
         genderWrapper.$(byText(value)).click();
         return this;
     }
-    public RegistrationPage number (String number) {
+
+    public RegistrationPage number(String number) {
         userNumberInput.setValue(number);
         return this;
     }
-    public RegistrationPage dateOfBirth (String day, String month, String year) {
+
+    public RegistrationPage dateOfBirth(String day, String month, String year) {
         dateOfBirthInput.click();
-        calendarComponent.setDate(day,month,year);
+        calendarComponent.setDate(day, month, year);
         return this;
     }
 
-    public RegistrationPage subjectsInput (String subjectsInput) {
+    public RegistrationPage subjectsInput(String subjectsInput) {
         subjects.setValue(subjectsInput).pressEnter();
         return this;
     }
-    public RegistrationPage hobbies (String value) {
+
+    public RegistrationPage hobbies(String value) {
         hobbies.$(byText(value)).click();
         return this;
     }
-    public RegistrationPage uploadPicture (String fileName) {
+
+    public RegistrationPage uploadPicture(String fileName) {
         uploadPicture.uploadFromClasspath(fileName);
         return this;
     }
-    public RegistrationPage address (String value) {
+
+    public RegistrationPage address(String value) {
         currentAddress.setValue(value);
         return this;
     }
-    public RegistrationPage state (String value) {
+
+    public RegistrationPage state(String value) {
         $("#state").click();
-        $("#stateCity-wrapper").$(byText(value)).click();
+        state.$(byText(value)).click();
         return this;
     }
-    public RegistrationPage city (String value) {
+
+    public RegistrationPage city(String value) {
         $("#city").click();
-        $("#stateCity-wrapper").$(byText(value)).click();
+        state.$(byText(value)).click();
         return this;
     }
-    public void submit () {
+
+    public void submit() {
         submit.click();
     }
 }
--- src/test/java/pages/components/StudentRegistrationFormAsserts.java
+++ src/test/java/pages/components/StudentRegistrationFormAsserts.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class StudentRegistrationFormAsserts {
-    public void checkResultForMaxFields(){
+    public void checkResultForMaxFields() {
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").$(byText("Student Name")).parent().shouldHave((text("Вася Пупкин")));
         $(".table-responsive").$(byText("Student Email")).parent().shouldHave((text("vasyaPupkin@mail.ru")));
@@ -18,11 +18,13 @@ public class StudentRegistrationFormAsserts {
         $(".table-responsive").$(byText("Address")).parent().shouldHave((text("Проспект танцующих слонов, 5")));
         $(".table-responsive").$(byText("State and City")).parent().shouldHave((text("NCR Delhi")));
     }
-    public void checkResultForMinFields(){
+
+    public void checkResultForMinFields() {
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
     }
-    public void checkResultWithoutRequiredParameter(){
+
+    public void checkResultWithoutRequiredParameter() {
         $("*").shouldNotHave(text("Thanks for submitting the form"));
 
     }
