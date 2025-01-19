d7199ccef66192270dce54c97e27c5c02a9b87b5
Igor Zakurdaev <zakurdaev12@mail.ru>
2025-01-19 17:39:38 +0400

    -added parameterizedTests

---
+++ src/test/java/ru/tuz/tests/TuzTests.java
@@ -0,0 +1,30 @@
+package ru.tuz.tests;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import ru.tuz.utils.Language;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TuzTests   {
+    @BeforeEach
+     void setUp(){
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        open("https://www.tuz-saratov.ru/");
+    }
+    @EnumSource(Language.class)
+    @ParameterizedTest
+    void CheckTitleTest(Language language){
+        $(".lang").$(byText(language.name())).click();
+        $(".main-page").shouldHave(text(language.description));
+    }
+}
---
+++ src/test/java/ru/tuz/utils/Language.java
@@ -0,0 +1,14 @@
+package ru.tuz.utils;
+
+public enum Language {
+    RUS("СЕГОДНЯ В ТЕАТРЕ"),
+
+    ENG("TODAY IN THE THEATER");
+
+    public final String description;
+
+    Language(String description) {
+        this.description = description;
+    }
+
+}
